c7c07fe529abce98a3d323b7277fc3af34c790bf
John Burns <bufo333@gmail.com>
2014-01-08 00:21:27 -0500

    This is the first commit with a basic working skeleton of the project.

---
+++ program.js
@@ -0,0 +1,24 @@
+// This is the main program file contents bellow
+
+// variable declarations
+
+var caDefault = {
+	sectionName: "[ CA default ]",
+	dir: "509CA",
+	certs: "/certs",
+	crl_dir: "/crl",
+	database: "/index.txt",
+	new_certs_dir: "/newcerts",
+	certificate: "/ca/new_ca.pem",
+	serial: "serial",
+	crl: "crl.pem",
+	private_key: "new_ca_pk.pem",
+	RANDFILE: ".rand",
+	x509_extensions: "usr_cert",
+	name_opt: "ca_default",
+	cert_opt: "ca_default",
+	default_days: "365",
+	default_crl_days: "30",
+	default_md: "sha1",
+	preserve: "no"
+	}
